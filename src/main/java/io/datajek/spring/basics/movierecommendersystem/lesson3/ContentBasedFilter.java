d12e919aceab6206a5ca967365131b8a7a15c589
ISParashkevov18 <ISParashkevov18@codingburgas.bg>
2022-10-24 13:54:04 +0300

    Initial commit

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson3/ContentBasedFilter.java
@@ -0,0 +1,8 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson3;
+public class ContentBasedFilter {
+    public String[] getRecommendations(String  movie){
+
+        //return movie recommendations
+        return new String[] {"Happy feet", "Ice Age", "Shark Tale"};
+    }
+}
